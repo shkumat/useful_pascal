a97a508a6caa551dc1df2e4527449ebc3da24421
unknown <alexey.shkumat@hotmail.com>
2023-08-02 19:22:04 +0300

    commit on Aug 2
    update _find0.pas_

--- find0.pas
+++ find0.pas
@@ -1,4 +1,4 @@
-const   ABOUT0= 'Filter of forbidden characters; ver.1.02 (2023-07-09); author Oleksii Shkumat.'
+const   ABOUT0= 'Filter of forbidden characters; ver.1.03 (2023-08-02); author Oleksii Shkumat.'
 ;       USAGE1= 'Usage : find0 [/?] [/t] [/n] [/v] [/f]   [InputFileName]  [OutputFileName]    '
 ;       USAGE2= '            /n   - print also line numbers                 ;                  '
 ;       USAGE3= '            /v   - inversion-mode - print only valid lines ;                  '
@@ -7,7 +7,7 @@ const   ABOUT0= 'Filter of forbidden characters; ver.1.02 (2023-07-09); author O
 ;       USAGE6= ' example:                                                                     '
 ;       USAGE7= '          type SomeFile.csv | find0  /t /v > OutputFile.csv                   '
 ;
-var     K               :   Integer
+var     I               :   Integer
 ;       S               :   ShortString
 ;       LineNum         :   LongInt     =   0
 ;       InputFile       :   Text
@@ -25,28 +25,25 @@ begin
     if InputFileName>'' then
         Result:=EOF(InputFile)
     else
-        Result:=EOF(Input);  
-end
-;
+        Result:=EOF(Input);
+end;
+
 procedure ProcessLine();
-var     Line            :   AnsiString  =   ''
-;       LineIsValid     :   Boolean     =   TRUE 
-;       I,J             :   Integer     ;
+var     Line        :  AnsiString = ''
+;       LineIsValid :  Boolean    = TRUE;
 begin
     if InputFileName>'' then
-        ReadLn(InputFile,Line)
+        ReadLn(InputFile, Line)
     else
         ReadLn(Line);
     Inc(LineNum);
     for I:=1 to Length(Line) do
-        for J:=0 to 31 do
-            if Line[I]=Chr(J) then
-                if (J<>9) or (not EnableTabChar) then
-                    if FixingMode then
-                        Line[I]:=#32
-                    else    
-                        LineIsValid:=FALSE;
-    if (LineIsValid<>InversionMode) and (not FixingMode) then
+        if ( Ord( Line[I] ) < 32 ) and ( ( Ord( Line[I] ) <> 9 ) or ( not EnableTabChar ) ) then
+                if FixingMode then
+                    Line[I]:=#32
+                else
+                    LineIsValid:=FALSE;
+    if ( LineIsValid<>InversionMode ) and ( not FixingMode ) then
         Exit;
     if OutputFileName>'' then
         if PrintLineNumbers then
@@ -58,43 +55,46 @@ begin
             WriteLn('Line ',LineNum,' : ',Line)
         else
             WriteLn(Line);
-end
-;
+end;
+
 begin
-    for K:=1 to ParamCount do
+    for I:=1 to ParamCount do
         begin
-            S:=LowerCase( ParamStr(K) );
+            S:=LowerCase( ParamStr(I) );
             if (S='?') or (S='/?') or (S='-?') or (S='/h') or (S='-h') or (S='-help') or (S='--help') then
                 begin
                     Writeln(ABOUT0);Writeln('');Writeln(USAGE1);Writeln(USAGE2);Writeln(USAGE3);
                     Writeln(USAGE4);Writeln(USAGE5);Writeln(USAGE6);Writeln(USAGE7);
                     Exit;
-                end;                   
-            if (S='/t') or (S='-t') then 
-               EnableTabChar:= TRUE;  
-            if (S='/n') or (S='-n') then 
-               PrintLineNumbers:= TRUE;  
-            if (S='/v') or (S='-v') then 
-               InversionMode:= TRUE;  
-            if (S='/f') or (S='-f') then 
-               FixingMode:= TRUE;  
-            if (S<>'/t') and (S<>'-t') and (S<>'/v') and (S<>'-v') and (S<>'/n') and (S<>'-n') and (S<>'/f') and (S<>'-f') then
-                if InputFileName = '' then
-                    InputFileName := S
+                end;
+            if (S='/t') or (S='-t') then
+               EnableTabChar:= TRUE;
+            if (S='/n') or (S='-n') then
+               PrintLineNumbers:= TRUE;
+            if (S='/v') or (S='-v') then
+               InversionMode:=TRUE;
+            if (S='/f') or (S='-f') then
+               FixingMode:=TRUE;
+            if (S[1]<>'/') and (S[1]<>'-') then
+                if InputFileName='' then
+                    InputFileName:=S
                 else
-                    OutputFileName:= S;
+                    OutputFileName:=S;
     end;
     if InputFileName>'' then
         begin
-            Assign(InputFile,InputFileName); 
+            Assign(InputFile, InputFileName);
             ReSet(InputFile);
             if OutputFileName>'' then
                 begin
-                    Assign(OutputFile,OutputFileName); 
+                    Assign(OutputFile, OutputFileName);
                     ReWrite(OutputFile);
                 end;
         end;
-    while not EndOfFile   do     ProcessLine;
-    if InputFileName>''   then   Close(InputFile);
-    if OutputFileName>''  then   Close(OutputFile);
+    while not EndOfFile do
+        ProcessLine;
+    if InputFileName>'' then
+        Close(InputFile);
+    if OutputFileName>'' then
+        Close(OutputFile);
 end.
